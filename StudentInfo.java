d3cced2db945ecbd6050a175ee88e43deb8db680
Aidan Kiser <97229306+kiseraidan@users.noreply.github.com>
2022-09-04 19:01:41 -0500

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,20 @@
+/** 
+* Simple program to print author name and previous computer courses.
+*
+* Module 1: Activity 1
+* @author Aidan Kiser - COMP1210 - 001
+* @version 22 August 2022
+*/
+public class StudentInfo
+{  
+   /**
+   * Prints author name and previous computer courses one time.
+   * @param args Command Line arguments (not used).
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Aidan Kiser");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   }
+}
