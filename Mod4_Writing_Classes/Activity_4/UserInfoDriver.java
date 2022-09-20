e9938f6add36dca3b3df39d103abb6c17c6422a3
Aidan Kiser <97229306+kiseraidan@users.noreply.github.com>
2022-09-20 11:41:13 -0500

    Add files via upload

---
+++ Mod4_Writing_Classes/Activity_4/UserInfoDriver.java
@@ -0,0 +1,29 @@
+/**
+* Create a userinfodriver class.
+*
+* @author Aidan Kiser - COMP1210 - 001
+* @version 13 September 2022
+*/
+public class UserInfoDriver {
+
+   /**
+   * Create a UserInfoDriver class.
+   *
+   * @param args command line arguments
+   */
+   public static void main(String[] args) {
+      UserInfo user1 = new UserInfo("Pat", "Doe");
+      System.out.println("\n" + user1);
+      user1.setLocation("Auburn");
+      user1.setAge(19);
+      user1.logOn();
+      System.out.println("\n" + user1);
+   
+      UserInfo user2 = new UserInfo("Sam", "Jones");
+      System.out.println("\n" + user2);
+      user2.setLocation("Atlanta");
+      user2.setAge(21);
+      user2.logOn();
+      System.out.println("\n" + user2);
+   }
+}
